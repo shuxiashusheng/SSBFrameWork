1dc674a3268c917287c7490cec6bfbd9ab78512c
shaobo10 <shaobo10@staff.sina.com.cn>
2018-08-31 20:27:26 +0800

    some files

---
+++ SSBC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSBC
+//
+//  Created by shaobo10 on 2018/8/31.
+//  Copyright © 2018年 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
